db32a7e795615f337c8101aeae8f63381c0ff8a5
soaresmilton <eng.milton.soares@gmail.com>
2021-10-21 18:16:19 -0300

    end stats

---
+++ stats.js
@@ -0,0 +1,30 @@
+const { stat } = require('fs');
+const os = require('os');
+
+const interval = 1000;
+
+setInterval(() => {
+  const { freemem, totalmem } = os;
+
+  const totalMemory = parseInt(totalmem() / 1024 / 1024);
+
+  const freeMemory = parseInt(freemem() / 1024 / 1024);
+
+  const usingMemory =  parseInt(totalMemory - freeMemory);
+
+  const percentsUsage = parseInt((usingMemory / totalMemory) * 100);
+
+  const avaibleMemory = parseInt((freeMemory / totalMemory) * 100);
+
+  const stats = {
+    Free_Memory: `${freeMemory} MB`,
+    Using_Memory: `${usingMemory} MB`,
+    Total_Memory: `${totalMemory} MB`,
+    Usage_Memory: `${percentsUsage} %`,
+    Avaible_Memory: `${avaibleMemory} %`
+  }
+  console.clear()
+  console.log("======= PC STATS =======");
+  console.table(stats);
+}, interval);
+
